9a91dd4288665fb632a43b387b30fc3392925be7
AnthonyP1947 <anthonyparker2k16@gmail.com>
2023-07-16 15:44:28 -0500

    declared new widgets in MainActivity and LuckyNumberActivity

--- app/src/main/java/com/mastercoding/luckynumgenapp/LuckyNumberActivity.java
+++ app/src/main/java/com/mastercoding/luckynumgenapp/LuckyNumberActivity.java
@@ -3,12 +3,21 @@ package com.mastercoding.luckynumgenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LuckyNumberActivity extends AppCompatActivity {
+    TextView welcomeTxt, luckyNumberTxt;
+    Button share_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lucky_number);
+
+        welcomeTxt = findViewById(R.id.textView2);
+        luckyNumberTxt = findViewById(R.id.lucky_number_txt);
+        share_btn = findViewById(R.id.share_number_btn);
     }
 }
--- app/src/main/java/com/mastercoding/luckynumgenapp/MainActivity.java
+++ app/src/main/java/com/mastercoding/luckynumgenapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.mastercoding.luckynumgenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
     }
 }
